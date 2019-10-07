9d7ed1a7352f01efc28ff8ad05276bcf1c5d92c2
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-10-07 14:58:38 +0700

    update file webpack environment file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,7 @@ const {environment} = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
 }))
 
