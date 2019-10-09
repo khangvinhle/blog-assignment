189f29f204037f31766757db6b80efe5967e59e8
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-10-09 12:23:36 +0700

    move login in application layout to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_authentication_links(class_name = nil)
+    if user_signed_in?
+      link_to 'Log out', destroy_user_session_path, method: :delete, class: class_name
+    else
+      link_to('Log in', new_user_session_path, class: class_name) +
+          link_to('Register', new_user_registration_path, class: class_name)
+    end
+  end
 end
