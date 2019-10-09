c8f191478a5b1bb27b312a324ed21c88cc40e226
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-10-09 11:19:43 +0700

    add menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users, path: 'accounts', controllers: {
       session: 'users/sessions',
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root 'home#index'
+  resources :users do
+    resource :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
